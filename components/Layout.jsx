0b6783d50d6a743c8708b14af3de4397a296b34e
Geeklord-Tazheeb <thshamsi@hotmail.com>
2022-05-17 17:53:11 +0530

    This is a new commit for what I originally planned to be amended

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>PHONIX store</title>
       </Head>
       <header>
         <Navbar />
